0b8a5b10885ae51d7431730fe720c0e0d33a5ea2
Archit Rungta <architrungta120@gmail.com>
2020-12-30 09:13:12 +0000

    done with small tests

--- hw4/mm_alloc.c
+++ hw4/mm_alloc.c
@@ -8,20 +8,21 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 struct list_node;
 
 struct list_node
 {
-  int free;
-  int size;
+  char free;
+  size_t size;
   struct list_node* prev;
   struct list_node* next;
-  int magic;
+  __uint16_t magic;
 };
 
-#define MAGIC 0xAB12C
-#define IS_MEM(x) (x->magic == MAGIC)
+#define MAGIC 0xAB12
+#define IS_MEM(x) ((x)->magic == MAGIC)
 
 static struct list_node root;
 
@@ -32,8 +33,9 @@ void init_root() {
   root.prev = NULL;
 }
 
-void split_node(struct list_node* iter, int sz)
+void split_node(struct list_node* iter, size_t sz)
 {
+  //printf("splitting node\n");
   struct list_node* pt2 = ((void*)iter + sizeof(struct list_node) + sz);
   pt2->prev = iter;
   pt2->next = iter->next;
@@ -41,49 +43,126 @@ void split_node(struct list_node* iter, int sz)
   pt2->size = iter->size - sizeof(struct list_node) - sz;
   pt2->free = 1;
   iter->size = sz;
+
+  if(iter->next)
+    iter->next->prev = pt2;
   iter->next = pt2;
+
 }
 
+void dump_nodes()
+{
+  struct list_node* iter = &root;
+  //printf("\n");
+  while(iter != NULL)
+  {
+    assert(IS_MEM(iter));
+    // printf("sz:%d cur:%x next:%x prev:%x free:%d\n", iter->size, iter, iter->next, iter->prev, iter->free);
+    iter = iter->next;
+  }
+  //printf("\n");
+
+}
+
+void* clear_mem(void* ptr, size_t sz)
+{
+  memset(ptr + sizeof(struct list_node), 0, sz);
+  return ptr + sizeof(struct list_node);
+}
 void* mm_malloc(size_t size)
 {
-  if(!root.magic == MAGIC)
+  if(root.magic != MAGIC)
     init_root();
-  
+  if(size == 0)
+    return NULL;
+
   struct list_node* iter = &root;
+  //printf("mallocing\n");
   while(1) {
+    //printf("sz:%d next:%x prev:%x free:%d\n", iter->size, iter->next, iter->prev, iter->free);
+
     if(iter->free && iter->size >= size) {
       if(iter->size > size+sizeof(struct list_node))
         split_node(iter, size);
 
       iter->free = 0;
-      return (void*)iter + sizeof(struct list_node);
+
+      dump_nodes();
+      return clear_mem(iter, size);
     }
     if(iter->next == NULL)  break;
     iter = iter->next;
   }
 
   // we reached the end and no memory found
-
+  //printf("calling sbrk\n");
   struct list_node* base = sbrk(0);
-  sbrk(sizeof(struct list_node) + size);
+  if(sbrk(sizeof(struct list_node) + size) == -1)
+    return NULL;
   base->next = NULL;
   base->magic = MAGIC;
   base->free = 0;
   base->prev = iter;
   base->size = size;
-  return (void*)base + sizeof(struct list_node);
+  iter->next = base;
+  return clear_mem(base, size);
+}
+
+size_t min(size_t x, size_t y)
+{
+  return x > y ? y : x;
 }
 
 void* mm_realloc(void* ptr, size_t size)
 {
-  //TODO: Implement realloc
+  if(size == 0 && ptr==NULL)  return NULL;
+  if(size == 0){
+    free(ptr);
+    return NULL;
+  }
+  if(ptr == NULL) return mm_malloc(size);
 
+  void* new_loc = mm_malloc(size);
+  if(new_loc==NULL)
+    return NULL;
+
+  memcpy(new_loc, ptr, min(size, (((struct list_node*)ptr) - 1)->size));
+  free(ptr);
   return NULL;
+}
+
+void collapse_any()
+{
+  struct list_node* iter = (&root)->next;
+  //printf("Looking to collapse\n");
+  while(iter != NULL)
+  {
+    assert(IS_MEM(iter));
+    //printf("sz:%d cur:%x next:%x prev:%x free:%d\n", iter->size, iter, iter->next, iter->prev, iter->free);
+    if(iter->free && iter->prev->free)
+    {
+      struct list_node* coal = iter->prev;
+      coal->size += sizeof(struct list_node) + iter->size;
+      coal->next = iter->next;
+      if(iter->next)
+        iter->next->prev = coal;
+      
+      //printf("collapsed!\n");
+    }
+    iter = iter->next;
+  }
 }
 
 void mm_free(void* ptr)
 {
-  //TODO: Implement free
+  if(ptr == NULL) return;
 
-  return NULL;
+  //printf("freeing\n");
+  struct list_node* meta = ((struct list_node*)ptr) - 1;
+  assert(IS_MEM(meta));
+  assert(!meta->free);
+  meta->free = 1;
+  collapse_any();
+
+  dump_nodes();
 }
--- hw4/mm_test.c
+++ hw4/mm_test.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define malloc mm_malloc
+#define free mm_free
 /* Function pointers to hw3 functions */
 void* (*mm_malloc)(size_t);
 void* (*mm_realloc)(void*, size_t);
@@ -38,10 +40,50 @@ void load_alloc_functions() {
 int main() {
     load_alloc_functions();
 
-    int *data = (int*) mm_malloc(sizeof(int));*data = (int*) mm_malloc(sizeof(int));*data = (int*) mm_malloc(sizeof(int));*data = (int*) mm_malloc(sizeof(int));
-    assert(data != NULL);
-    (data -2)[0] = 0x162;
+    int *data = (int*) mm_malloc(sizeof(int));
+    int *data2 = (int*) mm_malloc(sizeof(int)*2);
+    int *data3 = (int*) mm_malloc(sizeof(int));
+    int *data4 = (int*) mm_malloc(sizeof(int));
     mm_free(data);
-    printf("malloc test successful! %d\n", data[0]);
+    mm_free(data2);
+    data = (int*) mm_malloc(8);
+    data[0] =6;
+    data[1] = 5;
+    mm_free(data4);
+    printf("malloc test successful! %d %d\n", data[0], data[1]);
+    mm_free(data);
+    mm_free(data3);
+    int* p = mm_malloc(5 * sizeof(int));
+    int i;
+    for(i = 0; i < 5; i++){
+        p[i] = i;
+    }
+    for(i = 0; i < 5; i++){
+        printf("address: %p, value: %d\n", p + i, p[i]);
+    }
+	mm_free(p);
+    p = mm_malloc(1);
+    mm_free(p);
+
+    printf("p has been freed\n");
+    p = malloc(sizeof(int));
+    *((int*)p) = 5;
+    printf("integer: address: %p, value: %d\n", p, *((int*)p));
+    free(p);
+    char* c = "Hello World"; 
+    p = malloc(sizeof(char) * (strlen(c) + 1));
+    memcpy(p, c, strlen(c));
+    printf("String: address: %p, value: %s\n", p, (char*)p);
+    free(p);
+    p = malloc(sizeof(float));
+    *((float*)p) = 1.2345;
+    printf("float: address: %p, value: %f\n", p, *((float*)p));
+    free(p);
+
+    // for(i = 1; (p = malloc(i)) != NULL; i*=2){
+    //     printf("%d bytes allocated\n", i);
+    //     free(p);
+    // }
+
     return 0;
 }
